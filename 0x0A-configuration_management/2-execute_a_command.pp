84bb051d95a68668681a271518be5a62fd7d211d
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-22 15:21:15 -0700

    threwe

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,15 @@
 # Puppet manifest to kill a process named "killmenow" using pkill
-exec {'killmenow':
-  command   =>  '/usr/bin/pkill killmenow',
-  provider  =>  'shell',
-  returns   =>  [0, 1],
+exec { 'killmenow':
+  command     => '/usr/bin/pkill killmenow',
+  refreshonly => true,
+  subscribe   => Exec['start_killmenow'],
+  returns     => [0, 1],
+}
+
+exec { 'start_killmenow':
+  command     => '/usr/bin/pkill killmenow',
+  user        => 'shell',            
+  logoutput   => true,    
+  refreshonly => true,
+  notify      => Exec['killmenow'],    
 }
