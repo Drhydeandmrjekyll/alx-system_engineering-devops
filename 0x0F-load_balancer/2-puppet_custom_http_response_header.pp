61825d994c84a817833f349be9b5eb767e6670ba
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-10-03 08:06:12 -0700

    https

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,43 +1,67 @@
 #!/usr/bin/env bash
-## Installs Nginx with puppet with the following configurations:
+## Installs Nginx with puppet with following configurations:
 #+    Listens on port 80.
-#+    Returns a page containing "Hello World!" when queried
+#+    Returns page containing "Hello World!" when queried
 #+     at the root with a curl GET request.
 #+    Configures /redirect_me as a "301 Moved Permanently".
 #+    Includes a custom 404 page containing "Ceci n'est pas une page".
 #+    Contains a custom HTTP header named X-Served-By.
-#+    The value of the HTTP header is the hostname of the running server.
+#+    Value of HTTP header is hostname of running server.
+# Install the puppetlabs-apt module to manage APT repositories
+# Combined Puppet Manifest
 
+# Define a custom fact to retrieve the hostname of the server
+Facter.add('server_hostname') do
+  setcode 'hostname'
+end
+
+# Install the puppetlabs-apt module to manage APT repositories
+class { 'apt':
+  update => {
+    frequency => 'daily',
+  },
+  purge_sources_list.d => true,
+}
+
+# Install Nginx package
 package { 'nginx':
   ensure => 'installed',
 }
 
-file { '/var/www/html':
-  ensure => directory,
+# Create a custom template for the Nginx configuration
+file { '/etc/nginx/default.erb':
+  ensure  => 'file',
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-}
-
-file { '/var/www/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page",
-}
-
+# Create a template for the Nginx configuration file
 file { '/etc/nginx/sites-available/default':
-  ensure  => present,
+  ensure => 'file',
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+# Create the custom HTTP response header in Nginx configuration
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => 'file',
+  content => "add_header X-Served-By $::server_hostname;\n",
+  notify  => Service['nginx'],
+}
+
+# Create the Nginx default site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
   content => '
     server {
       listen 80 default_server;
       listen [::]:80 default_server;
-      add_header X-Served-By '255935-lb-01';
+      add_header X-Served-By $::server_hostname;
       root /var/www/html;
       index index.html index.htm;
 
       location /redirect_me {
-        return 301 https//youtube.com/;
+        return 301 https://youtube.com/;
       }
 
       error_page 404 /404.html;
@@ -47,10 +71,10 @@ file { '/etc/nginx/sites-available/default':
       }
     }
   ',
+  notify  => Service['nginx'],
 }
-
+# Ensure the Nginx service is running and enabled
 service { 'nginx':
-  ensure  => running,
+  ensure  => 'running',
   enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
 }
