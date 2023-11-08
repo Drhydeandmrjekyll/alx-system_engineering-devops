8495aeef5d999700707db2645f19994aca3ea372
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-11-08 00:29:30 +0000

    recurse_it

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+"""Query Reddit for top ten posts"""
 
 import requests
 
+
 def top_ten(subreddit):
     """
-    Function to query Reddit API and print the titles of first 10 hot posts for a given subreddit.
-    Args:
-        subreddit (str): Name of subreddit to query.
-
-    Returns:
-        None
+    Function to query the Reddit API and print the titles of the first 10 hot posts for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'MyBot/1.0'}  # Set a custom User-Agent to avoid Too Many Requests error.
 
-    response = requests.get(url, headers=headers)
+    # Reddit API endpoint for getting top posts
+    url = "https://www.reddit.com/r/{subreddit}/hot.json"
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if posts:
-            for post in posts:
-                print(post['data']['title'])
+    # Set custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    try:
+        # Send GET request to Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json().get('data', {}).get('children', [])
+
+            # Print titles of first 10 hot posts
+            for child in data[:10]:
+                post_data = child.get('data', {})
+                print(post_data.get('title'))
         else:
-            print("No posts found in the subreddit.")
-    else:
-        print("Not a valid subreddit or an issue with the Reddit API.")
+            print("None")
+    except requests.RequestException as e:
+        print("None")
 
-if __name__ == '__main__':
-    import sys
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit: ")
+    top_ten(subreddit)
