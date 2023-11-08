633d53b873a3459659e24d249c9a212e9ddef9cc
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-11-08 00:15:36 +0000

    count_it

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,50 +1,66 @@
 #!/usr/bin/python3
+"""
+100-count
+"""
 
 import requests
 
-def count_words(subreddit, word_list, found_list=[], after=None):
-    """ 
+def count_words(subreddit, word_list, after=None, counts=None):
+    """
     Recursive function to query Reddit API and count keywords in titles of hot articles.
     Args:
         subreddit (str): Name of subreddit to query.
         word_list (list): List of keywords to count.
         after (str): 'after' parameter for pagination.
-        counts (dict): A dictionary to store word counts.
+        counts (dict): Dictionary to store word counts.
 
     Returns:
         None
     """
-    user_agent = {'User-agent': 'test45'}
-    posts = requests.get('http://www.reddit.com/r/{subreddit}/hot.json?after={after}', headers=user-agent)
+    if counts is None:
+        counts = {}
 
-    if after is None:
-      word_list = [word.lower() for word in word_list]
+    if not word_list:
+        return print_results(counts)
 
-    if posts.status_code == 200:
-      posts = posts.json()['data']
-      aft = posts['after']
-      posts = posts['children']
+    keyword = word_list.pop()
+    url = 'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
+    headers = {'User-Agent': 'MyBot/1.0'}  # Set custom User-Agent to avoid Too Many Requests error.
+    params = {'after': after} if after else {}
 
-      for post in posts:
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return
+
+    data = response.json()
+    posts = data['data']['children']
+
+    for post in posts:
         title = post['data']['title'].lower()
+        keyword = keyword.lower()
+        if keyword in title and not title.startswith(keyword + '.'):
+            counts[keyword] = counts.get(keyword, 0) + 1
 
-        for word in title.split(' '):
-          if word in word_list:
-            found_list.append(word)
-      
-      if aft is not None:
-        count_words(subreddit, word_list, found_list, aft)
-      else:
-        result = {}
+    after = data['data']['after']
 
-        for word in found_list:
-          if word.lower() in result:
-            result[word.lower()] += 1
-          else:
-            result[word.lower()] = 1
+    if after:
+        return count_words(subreddit, word_list, after, counts)
+    else:
+        return print_results(counts)
 
-      for key, value in sorted(result.items(), key=lambda item: item[1], reverse=True):
-        print('{key}: {value}')
-  
-else:
-  return
+def print_results(counts):
+    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print("{word}: {count}")
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        keywords = [x for x in sys.argv[2].split()]
+        count_words(subreddit, keywords)
